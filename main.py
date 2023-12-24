bcb80f048225781c81f55da562817cc95dfb1820
kyevsg <88254707+kyevsg@users.noreply.github.com>
2023-12-24 20:20:00 +0000

    changed app.py to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import random
 from secret_santa import *
 
 app = Flask(__name__)
@@ -14,6 +15,16 @@ def secretsanta():
 @app.route('/wishlists')
 def wishlists():
   return render_template('wishlists.html')
-
+  
+"""
+# use if not running on repl
 if __name__ == '__main__':
   app.run()
+"""
+
+# use if running on repl
+if __name__ == "__main__":  
+  app.run( # Starts the site
+    host='0.0.0.0',  # sStablishes the host, required for repl to detect the site
+    port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+)
